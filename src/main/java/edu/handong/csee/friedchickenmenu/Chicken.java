5533f44c629a6987f3210218ca595d7d80a4cfa2
leehyunji0715 <leehyunji0715@172.17.220.101>
2018-04-02 22:50:35 +0900

    each Chicken and Controller classes' comment

--- src/main/java/edu/handong/csee/friedchickenmenu/Chicken.java
+++ src/main/java/edu/handong/csee/friedchickenmenu/Chicken.java
@@ -1,31 +1,53 @@
 package edu.handong.csee.friedchickenmenu;
+/**
+ * This class stores Chicken's name, its price and ratings
+ * This class has mName, mPrice and mRatings members
+ * mName is String class type, and it stores chicken's name
+ * mPrice is integer type, and it has chicken's price value
+ * mRatings is integer type and it is ratings of the chicken
+ * This class has Constructor, getmName(), setmName(), getmPrice(), setmPrice(), getmRatings(), setmRatings() methods
+ * In Constructor, by inputting (String)name, (int)price and (int) ratings, initialize called class
+ * getmName() input void and returns mName
+ * setmName() input mName to this.mName and returns nothing
+ * getmPrice() input void and returns mPrice
+ * setmPrice() input mPrice to this.mPrice and returns nothing
+ * getmRatings() input void and returns mRatings
+ * setmRatings() input mRatings to this.mRatings and returns nothing
+ * @author leehyunji0715
+ *
+ */
 
-public class Chicken {
-	String mName = new String();
-	int mPrice;
-	int mRatings;
-	public Chicken(String Name,int Price,int Ratings) {
-		this.mName = Name;
-		this.mPrice = Price;
-		this.mRatings = Ratings;
+public class Chicken {//This is public class named Chicken
+	String mName = new String();//Instantiate String class type mname
+	int mPrice;//call integer 'mPrice'
+	int mRatings;//call integer 'mRatings'
+	public Chicken() {//Instantiate Chicken class' instance variable to base value
+		mName = "\0";//initialize nName nothing
+		mPrice = 0;//initialize nPrice 0
+		mRatings = 0;//initialize nRatings 0
 	}
-	public String getmName() {
-		return mName;
+	public Chicken(String name,int price,int ratings) {//This is public class named Chicken and input String name, integer price and integer ratings
+		this.mName = name;//input name which is String class type to this.mName
+		this.mPrice = price;//input price which is integer type to this.mPrice
+		this.mRatings = ratings;//input ratings which is integer type to this.mRatings
 	}
-	public void setmName(String nName) {
-		this.mName = nName;
+	public String getmName() {// this is getter which gets mName String value
+		return mName;// return mName String
 	}
-	public int getmPrice() {
-		return mPrice;
+	public void setmName(String nName) {//this is setter which stores nName String to this.mName
+		this.mName = nName;//allocate nName String value to this.mName
 	}
-	public void setmPrice(int mPrice) {
-		this.mPrice = mPrice;
+	public int getmPrice() {// this is getter which gets mPrice integer value
+		return mPrice;//return mPrice integer value
 	}
-	public int getmRatings() {
-		return mRatings;
+	public void setmPrice(int nPrice) {//this is setter which stores nPrice integer to this.mPrice
+		this.mPrice = nPrice;//allocate nPrice integer value to this.mPrice
 	}
-	public void setmRatings(int mRatings) {
-		this.mRatings = mRatings;
+	public int getmRatings() {// this is getter which gets mRatings integer value
+		return mRatings;//return mRatings integer value
+	}
+	public void setmRatings(int nRatings) {//this is setter which stores nRatings integer to this.mRatings
+		this.mRatings = nRatings;//allocate nRatings integer value to this.mRatings
 	}
 	
 }
--- src/main/java/edu/handong/csee/friedchickenmenu/Controller.java
+++ src/main/java/edu/handong/csee/friedchickenmenu/Controller.java
@@ -1,22 +1,27 @@
 package edu.handong.csee.friedchickenmenu;
+/**
+ * This class is for operating 'Chicken'class and shows chickens' name and their ratings
+ * This class has main() method
+ * @author leehyunji0715
+ *
+ */
+public class Controller {//This class is public named Controller
 
-public class Controller {
+	public static void main(String[] args) {//This is main method
+		Chicken menu1 = new Chicken("Cheese_mustard",16000,1);//Instantiate Chicken class named menu1 and initialize with "Cheese_mustard",16000,1 
+		Chicken menu2 = new Chicken("Honey_mustard",16000,1);//Instantiate Chicken class named menu2 and initialize with "Honey_mustard",16000,1
+		Chicken menu3 = new Chicken("Spicey_mustard",16000,1);//Instantiate Chicken class named menu3 and initialize with "Spicey_mustard",16000,1
 
-	public static void main(String[] args) {
-		Chicken menu1 = new Chicken("Cheese_mustard",16000,1);
-		Chicken menu2 = new Chicken("Honey_mustard",16000,1);
-		Chicken menu3 = new Chicken("Spiey_mustard",16000,1);
+		menu1.setmRatings(3);//input rating value(3) to menu1's this.mRatings
+		menu2.setmRatings(4);//input rating value(4) to menu2's this.mRatings
+		menu3.setmRatings(1);//input rating value(1) to menu3's this.mRatings
 
-		menu1.setmRatings(3);
-		menu2.setmRatings(4);
-		menu3.setmRatings(1);
-
-		menu1.mRatings = menu1.getmRatings();
-		menu2.mRatings = menu2.getmRatings();
-		menu3.mRatings = menu3.getmRatings();
-		System.out.println(menu1.mName+"'s rating is "+menu1.mRatings);
-		System.out.println(menu2.mName+"'s rating is "+menu2.mRatings);
-		System.out.println(menu3.mName+"'s rating is "+menu3.mRatings);
+		menu1.mRatings = menu1.getmRatings();//give rating value(3) to menu1.mRatings
+		menu2.mRatings = menu2.getmRatings();//give rating value(4) to menu2.mRatings
+		menu3.mRatings = menu3.getmRatings();//give rating value13) to menu3.mRatings
+		System.out.println(menu1.mName+"'s rating is "+menu1.mRatings);//print out menu1's name and its own rating
+		System.out.println(menu2.mName+"'s rating is "+menu2.mRatings);//print out menu2's name and its own rating
+		System.out.println(menu3.mName+"'s rating is "+menu3.mRatings);//print out menu3's name and its own rating
 	}
 
 }
