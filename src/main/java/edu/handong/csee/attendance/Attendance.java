c8c15c02b3452872d27f84991533a09fffb8a07a
leehyunji0715 <leehyunji0715@gmail.com>
2018-04-08 16:54:54 +0900

    push

--- src/main/java/edu/handong/csee/attendance/Attendance.java
+++ src/main/java/edu/handong/csee/attendance/Attendance.java
@@ -1,6 +1,17 @@
 package edu.handong.csee.attendance;
-import java.util.Random;
-
+import java.util.Random;//Import java.util.Random to use random integer 0 to 10
+/**
+ * This class shows student's status about a course
+ * This class has mName, mGrade, mRandAbsence members
+ * mName is String class type and stands for student's name
+ * mGrade is integer and this is student's grade
+ * mRandAbsence is integer which stands for student's number of absence
+ * This class has Constructor and printResult() method
+ * Constructor
+ * printResult()
+ * @author leehyunji0715
+ *
+ */
 public class Attendance {
 	Random randomGen = new Random();
 	String mName;
@@ -19,9 +30,11 @@ public class Attendance {
 	void printResult() {
 		if(mRandAbsence>=6) {
 			System.out.println("I’m sorry, "+ mName + ". You failed this course");
+			System.out.println(mName + ". You didn't attend class "+mRandAbsence+" times\n");
 		}
 		else {
 			System.out.println("Congratulation "+mName+"! you passed this course!");
+			System.out.println(mName + ". You didn't attend class "+mRandAbsence+" times\n");
 		}
 	}
 }
